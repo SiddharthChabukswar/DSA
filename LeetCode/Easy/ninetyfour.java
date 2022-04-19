1092eb58cd55ddc44f8f61e7ed4f45cd292e9f1d
SiddharthChabukswar <siddharthchabukswar360@gmail.com>
2022-04-19 11:56:04 +0530

    19-04-2022

--- LeetCode/Easy/ninetyfour.java
+++ LeetCode/Easy/ninetyfour.java
@@ -1,6 +1,9 @@
 package LeetCode.Easy;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+import java.util.Stack;
 
 /*
 
@@ -43,12 +46,86 @@ class TreeNode {
 }
 
 public class ninetyfour {
+
+	public TreeNode createTree(int root_val, Scanner sc) {
+		TreeNode root;
+		if(root_val == -1) return null;
+		root = new TreeNode(root_val);
+		TreeNode currnode, leftnode, rightnode;
+		int new_val;
+		Stack<TreeNode> node_stack = new Stack<TreeNode>();
+		node_stack.add(root);
+		while(node_stack.size() != 0){
+			currnode = node_stack.pop();
+			if(currnode == null) continue;
+
+			System.out.printf("\nEnter left node of %d: ", currnode.val);
+			new_val = sc.nextInt();
+			if(new_val == -1) leftnode = null;
+			else leftnode = new TreeNode(new_val);
+
+			System.out.printf("\nEnter right node of %d: ", currnode.val);
+			new_val = sc.nextInt();
+			if(new_val == -1) rightnode = null;
+			else rightnode = new TreeNode(new_val);
+
+			currnode.left = leftnode;
+			currnode.right = rightnode;
+
+			node_stack.add(leftnode);
+			node_stack.add(rightnode);
+		}
+
+		return root;
+	}
 	
-	public List<Integer> inorderTraversal(TreeNode root) {
-		return null;
+	
+	// Recursive Solution O(n)
+	/*
+	// Print inorder string O(n)
+	public void inorder(TreeNode curr){
+		if(curr == null) return;
+		inorder(curr.left);
+		System.out.println(curr.val);
+		inorder(curr.right);
 	}
 
+	// Get inorder List O(n)
+	public List<Integer> inorder(TreeNode curr, List<Integer> inorder_List){
+		if(curr == null) return inorder_List;
+		inorder_List = inorder(curr.left, inorder_List);
+		inorder_List.add(curr.val);
+		inorder_List = inorder(curr.right, inorder_List);
+		return inorder_List;
+	}
+
+	// Call recursive function O(n)
+	public List<Integer> inorderTraversal(TreeNode root) {
+		List<Integer> inorder_List = new ArrayList<Integer>();
+		inorder_List = inorder(root, inorder_List);
+		return inorder_List;
+	}
+
+	*/
+
+
+	// Iterative Solution O(n)
+
+	// call iterative function O(n)
+	public List<Integer> inorderTraversal(TreeNode root) {
+		List<Integer> inorder_List = new ArrayList<Integer>();
+		return inorder_List;
+	}
+
+
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter root: ");
+		int root_val = sc.nextInt();
+		ninetyfour objNinetyfour = new ninetyfour();
+		TreeNode root = objNinetyfour.createTree(root_val, sc);
+		List<Integer> inorder_list = objNinetyfour.inorderTraversal(root);
+		System.out.println(inorder_list);
+		sc.close();
 	}
 }
