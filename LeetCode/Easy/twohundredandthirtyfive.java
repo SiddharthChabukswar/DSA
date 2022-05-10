477752a47dc28e01bb2715679a440b632bfc7039
SiddharthChabukswar <siddharthchabukswar360@gmail.com>
2022-05-10 12:06:05 +0530

    10-05-2022

--- LeetCode/Easy/twohundredandthirtyfive.java
+++ LeetCode/Easy/twohundredandthirtyfive.java
@@ -162,7 +162,7 @@ public class twohundredandthirtyfive {
 	*/
 
 
-	// Solution for Binary Search Tree: O(n)
+	// Solution for Binary Search Tree: Recursive O(n)
 	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
 		if(root == null) return root;
 		if(root.val > p.val && root.val > q.val) return lowestCommonAncestor(root.left, p, q);
