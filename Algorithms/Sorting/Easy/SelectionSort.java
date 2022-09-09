0b64c5be708e7d74bcbb085f0294dfff0a978d7b
SiddharthChabukswar <siddharthchabukswar360@gmail.com>
2022-09-09 21:25:43 +0530

    09-09-2022

--- Algorithms/Sorting/Easy/SelectionSort.java
+++ Algorithms/Sorting/Easy/SelectionSort.java
@@ -1,5 +1,45 @@
 package Algorithms.Sorting.Easy;
 
+/*
+
+Purpose: Sort an unsorted array
+Time complexity: O(n)
+
+Approach:
+
+1. Find the minimum element from the current location and place it with the current location
+2. Used two nested loops
+
+{ 55, 42, 14, 96, 24, 5, 988, -55, 24}
+
+*/
+
 public class SelectionSort {
+
+	private static void performSelectionSort(int[] array) {
+		int i, j, temp, local_min, n = array.length;
+		for(i=0; i<n; i++) {
+			local_min = array[i];
+			for(j=i+1; j<n; j++) {
+				if(array[j] < local_min) {
+					local_min = array[j];
+					temp = array[i];
+					array[i] = array[j];
+					array[j] = temp;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		int[] array = new int[]{ 55, 42, 14, 96, 24, 5, 988, -55, 24};
+		System.out.print("Unsorted array: ");
+		for(int element: array) System.out.print(element + ", ");
+		System.out.println();
+		performSelectionSort(array);
+		System.out.print("Sorted array: ");
+		for(int element: array) System.out.print(element + ", ");
+		System.out.println();
+	}
 	
 }
