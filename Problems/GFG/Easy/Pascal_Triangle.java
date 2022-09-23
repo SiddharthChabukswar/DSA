32953c9775211c7b8f2a680583ce8c3e3dad50e1
SiddharthChabukswar <siddharthchabukswar360@gmail.com>
2022-09-23 10:47:48 +0530

    23-09-2022

---
+++ Problems/GFG/Easy/Pascal_Triangle.java
@@ -0,0 +1,74 @@
+package GFG.Easy;
+
+import java.util.ArrayList;
+
+/*
+
+https://practice.geeksforgeeks.org/problems/pascal-triangle0652/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=pascal-triangle
+
+Given a positive integer N, return the Nth row of pascal's triangle.
+Pascal's triangle is a triangular array of the binomial coefficients formed by summing up the elements of previous row.
+
+Example :
+1
+1 1
+1 2 1
+1 3 3 1
+For N = 3, return 3rd row i.e 1 2 1
+
+Example 1:
+
+Input:
+N = 4
+Output: 1 3 3 1
+Explanation: 4th row of pascal's triangle
+is 1 3 3 1.
+Example 2:
+
+Input:
+N = 5
+Output: 1 4 6 4 1
+Explanation: 5th row of pascal's triangle
+is 1 4 6 4 1.
+ 
+
+Your Task:
+Complete the function nthRowOfPascalTriangle() which takes n, as input parameters and returns an array representing the answer. The elements can be large so return it modulo 109 + 7. You don't to print answer or take inputs.
+
+Expected Time Complexity: O(N2)
+Expected Auxiliary Space: O(N2)
+
+Constraints:
+1 ≤ N ≤ 1000
+
+*/
+
+public class Pascal_Triangle {
+
+	ArrayList<Long> nthRowOfPascalTriangle(int n) {
+		ArrayList<ArrayList<Long>> pascalTriangle = new ArrayList<ArrayList<Long>>();
+		ArrayList<Long> pascalTriangleRow;
+		ArrayList<Long> pascalTrianglePrevRow;
+		Long sum;
+		pascalTriangleRow = new ArrayList<Long>();
+		pascalTriangleRow.add(1l);
+		pascalTriangle.add(pascalTriangleRow);
+		pascalTriangleRow = new ArrayList<Long>();
+		pascalTriangleRow.add(1l);
+		pascalTriangleRow.add(1l);
+		pascalTriangle.add(pascalTriangleRow);
+		for(int i=2; i<n; i++) {
+			pascalTrianglePrevRow = pascalTriangle.get(i-1);
+			pascalTriangleRow = new ArrayList<Long>();
+			pascalTriangleRow.add(1l);
+			for(int j=0; j<i-1; j++) {
+				sum = (pascalTrianglePrevRow.get(j) + pascalTrianglePrevRow.get(j+1))%10000007l;
+				pascalTriangleRow.add(sum);
+			}
+			pascalTriangleRow.add(1l);
+			pascalTriangle.add(pascalTriangleRow);
+		}
+		return pascalTriangle.get(n-1);
+	}
+
+}
