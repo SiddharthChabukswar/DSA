a4af4174b44d03df95083d26250cbb7da92ee7e8
SiddharthChabukswar <siddharthchabukswar360@gmail.com>
2022-09-28 10:06:38 +0530

    28-09-2022

---
+++ Problems/GFG/Medium/Count_Inversions.java
@@ -0,0 +1,57 @@
+package GFG.Medium;
+
+/*
+
+https://practice.geeksforgeeks.org/problems/inversion-of-array-1587115620/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=inversion-of-array
+
+Given an array of integers. Find the Inversion Count in the array. 
+
+Inversion Count: For an array, inversion count indicates how far (or close) the array is from being sorted. If array is already sorted then the inversion count is 0. If an array is sorted in the reverse order then the inversion count is the maximum. 
+Formally, two elements a[i] and a[j] form an inversion if a[i] > a[j] and i < j.
+ 
+
+Example 1:
+
+Input: N = 5, arr[] = {2, 4, 1, 3, 5}
+Output: 3
+Explanation: The sequence 2, 4, 1, 3, 5 
+has three inversions (2, 1), (4, 1), (4, 3).
+Example 2:
+
+Input: N = 5
+arr[] = {2, 3, 4, 5, 6}
+Output: 0
+Explanation: As the sequence is already 
+sorted so there is no inversion count.
+Example 3:
+
+Input: N = 3, arr[] = {10, 10, 10}
+Output: 0
+Explanation: As all the elements of array 
+are same, so there is no inversion count.
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function inversionCount() which takes the array arr[] and the size of the array as inputs and returns the inversion count of the given array.
+
+Expected Time Complexity: O(NLogN).
+Expected Auxiliary Space: O(N).
+
+Constraints:
+1 ≤ N ≤ 5*105
+1 ≤ arr[i] ≤ 1018
+
+*/
+
+public class Count_Inversions {
+
+	// Linear search O(N^2) (T.L.E.)
+	static long inversionCount(long arr[], long N) {
+		long answer = 0l;
+		for(int i=0; i<N; i++) {
+			for(int j=i+1; j<N; j++) {
+				if(arr[i] > arr[j]) answer++;
+			}
+		}
+		return answer;
+	}
+
+}
