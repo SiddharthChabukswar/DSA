a850fd7444a783350755182b31f7d57fa0224af2
SiddharthChabukswar <siddharthchabukswar360@gmail.com>
2022-09-29 17:16:31 +0530

    29-09-2022

---
+++ Problems/GFG/Easy/Floor_in_a_Sorted_Array.java
@@ -0,0 +1,53 @@
+package GFG.Easy;
+
+/*
+
+https://practice.geeksforgeeks.org/problems/floor-in-a-sorted-array-1587115620/1?track=DSASP-Searching&amp%3BbatchId=154&utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=floor-in-a-sorted-array
+
+Given a sorted array arr[] of size N without duplicates, and given a value x. Floor of x is defined as the largest element K in arr[] such that K is smaller than or equal to x. Find the index of K(0-based indexing).
+
+Example 1:
+
+Input:
+N = 7, x = 0 
+arr[] = {1,2,8,10,11,12,19}
+Output: -1
+Explanation: No element less 
+than 0 is found. So output 
+is "-1".
+Example 2:
+
+Input:
+N = 7, x = 5 
+arr[] = {1,2,8,10,11,12,19}
+Output: 1
+Explanation: Largest Number less than 5 is
+2 (i.e K = 2), whose index is 1(0-based 
+indexing).
+Your Task:
+The task is to complete the function findFloor() which returns an integer denoting the index value of K or return -1 if there isn't any such number.
+
+Expected Time Complexity: O(log N).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+1 ≤ N ≤ 107
+1 ≤ arr[i] ≤ 1018
+0 ≤ X ≤ arr[n-1]
+
+*/
+
+public class Floor_in_a_Sorted_Array {
+
+	static int findFloor(long arr[], int n, long x) {
+		int low = 0, high = n-1, mid;
+		while(low <= high) {
+			mid = (low + high)/2;
+			if(arr[mid] == x) return mid;
+			if(arr[mid] > x) high = mid-1;
+			else low = mid+1;
+		}
+		return high;
+	}
+
+}
