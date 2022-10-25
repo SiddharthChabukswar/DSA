88866c82b4de74afa4d98b051e6c60de715d017d
SiddharthChabukswar <siddharthchabukswar360@gmail.com>
2022-10-25 11:10:04 +0530

    25-10-2022

---
+++ Problems/GFG/Medium/Largest_Sum_Subarray_of_Size_at_least_K.java
@@ -0,0 +1,55 @@
+package GFG.Medium;
+
+/*
+
+https://practice.geeksforgeeks.org/problems/largest-sum-subarray-of-size-at-least-k3121/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=largest-sum-subarray-of-size-at-least-k
+
+Given an array and a number k, find the largest sum of the subarray containing at least k numbers. It may be assumed that the size of array is at-least k.
+ 
+
+Example 1:
+
+Input : 
+n = 4
+arr[] = {-4, -2, 1, -3}
+k = 2
+Output : 
+-1
+Explanation :
+The sub array is {-2, 1}
+ 
+Example 2:
+Input :
+n = 6 
+arr[] = {1, 1, 1, 1, 1, 1}
+k = 2
+Output : 
+6
+Explanation :
+The sub array is {1, 1, 1, 1, 1, 1}
+ 
+
+Your Task:  
+You don't need to read input or print anything. Your task is to complete the function maxSumWithK() which takes the array A[], its size N and an integer K as inputs and returns the value of the largest sum of the subarray containing at least k numbers.
+
+ 
+
+Expected Time Complexity: O(N)
+Expected Auxiliary Space: O(N)
+
+ 
+
+Constraints:
+1<=n<=105
+-105<=a[i]<=105
+1<=k<=n
+
+*/
+
+public class Largest_Sum_Subarray_of_Size_at_least_K {
+
+	public long maxSumWithK(long a[], long n, long k) {
+		return 0l;
+	}
+
+}
