96d94add4ed2cd77644c3f7369b46d8fa08d318c
SiddharthChabukswar <siddharthchabukswar360@gmail.com>
2022-11-10 13:02:15 +0530

    10-11-2022

---
+++ Problems/GFG/Utils/Node.java
@@ -0,0 +1,16 @@
+package GFG.Utils;
+
+/*
+ * Representation class of a linkedList
+*/
+
+public class Node {
+	
+	public int data;
+	public Node next;
+	public Node(int x) {
+		data = x;
+		next = null;
+	}
+
+}
