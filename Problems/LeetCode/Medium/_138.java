023514040668205b78ed21e319ca732cff97d93f
Siddharth Chabukswar <siddharthchabukswar360@gmail.com>
2025-01-26 22:42:54 +0530

    copy random list

--- Problems/LeetCode/Medium/_138.java
+++ Problems/LeetCode/Medium/_138.java
@@ -20,34 +20,38 @@ class Node {
 public class _138 {
 	
 	public Node copyRandomList(Node head) {
-		if(head == null) return null;
-		Node currNode = head, newNode = null, newHead = null;
-		while(currNode != null) {
+		if (head == null) return null;
+		
+		// handle nexts
+		Node currNode = head, newNode;
+		while (currNode != null) {
 			newNode = new Node(currNode.val);
 			newNode.next = currNode.next;
 			currNode.next = newNode;
 			currNode = newNode.next;
 		}
-		newHead = head.next;
+
+		// handle randoms
 		currNode = head;
-		while(currNode != null) {
+		while (currNode != null) {
 			newNode = currNode.next;
-			if(currNode.random != null) newNode.random = currNode.random.next;
+			if (currNode.random != null) newNode.random = currNode.random.next;
+			else newNode.random = null;
 			currNode = newNode.next;
 		}
+
+		// separate the lists
 		currNode = head;
-		newNode = currNode.next;
-		Node nextCurrNode = newNode.next, nextNewNode = null;
-		while(currNode != null) {
-			if(nextCurrNode == null) nextNewNode = null;
-			else nextNewNode = nextCurrNode.next;
-			currNode.next = nextCurrNode;
-			currNode = nextCurrNode;
-			if(nextNewNode == null) nextCurrNode = null;
-			else nextCurrNode = nextNewNode.next;
-			newNode.next = nextNewNode;
-			newNode = nextNewNode;
+		Node headClone = currNode.next;
+		while (currNode != null) {
+			newNode = currNode.next;
+			currNode.next = newNode.next;
+			currNode = currNode.next;
+			if (currNode != null) newNode.next = currNode.next;
+			else newNode.next = null;
 		}
-		return newHead;
+
+		return headClone;
 	}
+
 }
