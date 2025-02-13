ed4561eaa58a59a5fbec77a489f3de9bba38083f
Siddharth Chabukswar <siddharthchabukswar360@gmail.com>
2025-02-13 15:34:50 +0530

    https://leetcode.com/problems/n-queens/description/ best approach

--- Problems/LeetCode/Hard/_51.java
+++ Problems/LeetCode/Hard/_51.java
@@ -34,6 +34,7 @@ Constraints:
 */
 public class _51 {
 
+	/*
 	private boolean isSafePosition(final int n, final int row, final int col, final char[][] queenPositions) {
 		// flip threat positions for column
 		for (int i=row; i>=0; i--) {
@@ -50,22 +51,10 @@ public class _51 {
 		return true;
 	}
 
-	private List<String> generatePositionString(final int n, final char[][] queenPositions) {
-		List<String> positionsStringList = new ArrayList<>();
-		StringBuilder stringBuilder;
-		for (int i=0; i<n; i++) {
-			stringBuilder = new StringBuilder();
-			for (int j=0; j<n; j++) {
-				stringBuilder.append(queenPositions[i][j]);
-			}
-			positionsStringList.add(stringBuilder.toString());
-		}
-		return positionsStringList;
-	}
-
 	/*
 	Recursion with backtracking O(N!*N^2)
 	*/
+	/*
 	private void placeQueenAtIndexRow(final int n, final int row, final List<List<String>> positionLists, final char[][] queenPositions) {
 		if (n == row) {
 			positionLists.add(generatePositionString(n, queenPositions));
@@ -84,7 +73,7 @@ public class _51 {
 			}
 		}
 	}
-
+	
 	public List<List<String>> solveNQueens(int n) {
 		List<List<String>> positionLists = new ArrayList<>();
 		char[][] queenPositions = new char[n][n];
@@ -96,5 +85,57 @@ public class _51 {
 		placeQueenAtIndexRow(n, 0, positionLists, queenPositions);
 		return positionLists;
 	}
+	*/
+
+	private List<String> generatePositionString(final int n, final char[][] queenPositions) {
+		List<String> positionsStringList = new ArrayList<>();
+		StringBuilder stringBuilder;
+		for (int i=0; i<n; i++) {
+			stringBuilder = new StringBuilder();
+			for (int j=0; j<n; j++) {
+				stringBuilder.append(queenPositions[i][j]);
+			}
+			positionsStringList.add(stringBuilder.toString());
+		}
+		return positionsStringList;
+	}
+
+	private void placeQueenAtIndexRow(final int n, final int row, final List<List<String>> positionLists, final boolean[] column, final boolean[] fwdDiagonal, final boolean[] bwdDiagonal, final char[][] queenPositions) {
+		if (n == row) {
+			positionLists.add(generatePositionString(n, queenPositions));
+			return;
+		}
+		for (int col=0; col<n; col++) {
+			if (column[col] == false && fwdDiagonal[row+col] == false && bwdDiagonal[n-1+row-col] == false) {
+				queenPositions[row][col] = 'Q';
+				column[col] = true;
+				fwdDiagonal[row+col] = true;
+				bwdDiagonal[n-1+row-col] = true;
+				placeQueenAtIndexRow(n, row+1, positionLists, column, fwdDiagonal, bwdDiagonal, queenPositions);
+				column[col] = false;
+				fwdDiagonal[row+col] = false;
+				bwdDiagonal[n-1+row-col] = false;
+				queenPositions[row][col] = '.';
+			}
+		}
+	}
 	
+	/*
+	Better solution avoiding iterative safe checks O(N!)
+	*/
+	public List<List<String>> solveNQueens(int n) {
+		List<List<String>> positionLists = new ArrayList<>();
+		boolean[] column, fwdDiagonal, bwdDiagonal;
+		column = new boolean[n];
+		fwdDiagonal = new boolean[2*n-1];
+		bwdDiagonal = new boolean[2*n-1];
+		char[][] queenPositions = new char[n][n];
+		for (int i=0;i<n;i++) {
+			for (int j=0;j<n;j++) {
+				queenPositions[i][j] = '.';
+			}
+		}
+		placeQueenAtIndexRow(n, 0, positionLists, column, fwdDiagonal, bwdDiagonal, queenPositions);
+		return positionLists;
+	}
 }
